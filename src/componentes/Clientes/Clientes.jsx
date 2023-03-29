fa7749de3a9fb5053b2af188cf73053bfa2546ec
samuel carrizo <samuelcarrizot@gmail.com>
2023-03-29 20:09:56 -0300

    se modificaron algunas clases

--- src/componentes/Clientes/Clientes.jsx
+++ src/componentes/Clientes/Clientes.jsx
@@ -5,11 +5,11 @@ const Clientes = (props) => {
                 <div className='cliente_Avatar'><img alt='error al cargar avatar' src={props.avatar} /></div>
                 <p>{props.nombre}</p>
                 <div className='cliente_Star'>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
-                    <i class="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
+                    <i className="fa fa-star"></i>
                 </div>
                 <p className='texto'>{props.texto}</p>
             </section>
